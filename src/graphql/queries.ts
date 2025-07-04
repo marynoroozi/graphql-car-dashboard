370c26c5cc02820f96fa27b0ff5e6ea05a20baac
mary <noroozi.m90@gmail.com>
2025-07-04 10:58:32 -0400

    Add new queries and mutation structure

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -14,3 +14,57 @@ export const GET_CARS = gql`
     }
   }
 `;
+
+export const GET_CAR_BY_ID = gql`
+  query GetCarById($id: ID!) {
+    car(id: $id) {
+      id
+      make
+      model
+      year
+      color
+      mobile
+      tablet
+      desktop
+    }
+  }
+`;
+
+export const GET_CARS_FILTERED = gql`
+  query GetCarsFiltered(
+    $make: String
+    $model: String
+    $year: Int
+    $color: String
+  ) {
+    carsFiltered(make: $make, model: $model, year: $year, color: $color) {
+      id
+      make
+      model
+      year
+      color
+      mobile
+      tablet
+      desktop
+    }
+  }
+`;
+
+export const ADD_CAR = gql`
+  mutation AddCar($input: CarInput!) {
+    addCar(input: $input) {
+      success
+      message
+      car {
+        id
+        make
+        model
+        year
+        color
+        mobile
+        tablet
+        desktop
+      }
+    }
+  }
+`;
