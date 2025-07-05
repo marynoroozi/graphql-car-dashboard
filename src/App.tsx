70b04b8045dea7c91aeee1918296fdb2af265f8e
mary <noroozi.m90@gmail.com>
2025-07-04 20:28:40 -0400

    Add toast notification and Yup and hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { ThemeProvider, CssBaseline } from "@mui/material";
+import { Toaster } from "react-hot-toast";
 import theme from "./theme";
 import Layout from "./components/common/Layout";
 import HomePage from "./pages/HomePage";
@@ -28,6 +29,40 @@ export default function App() {
               </Routes>
             </Layout>
           </Router>
+
+          {/* Global Toast Notifications */}
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              duration: 4000,
+              style: {
+                background: "#363636",
+                color: "#fff",
+                borderRadius: "8px",
+                fontFamily: theme.typography.fontFamily,
+              },
+              success: {
+                iconTheme: {
+                  primary: theme.palette.success.main,
+                  secondary: "#fff",
+                },
+                style: {
+                  background: theme.palette.success.light,
+                  color: theme.palette.success.contrastText,
+                },
+              },
+              error: {
+                iconTheme: {
+                  primary: theme.palette.error.main,
+                  secondary: "#fff",
+                },
+                style: {
+                  background: theme.palette.error.light,
+                  color: theme.palette.error.contrastText,
+                },
+              },
+            }}
+          />
         </ErrorBoundary>
       </ApolloProvider>
     </ThemeProvider>
