3ecc844b10c38dab1c8bfb38d71163993842b739
mary <noroozi.m90@gmail.com>
2025-07-06 21:00:28 -0400

    Just in Dev mode, use MSW to mock API calls

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,17 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { worker } from "./mocks/browser";
 
-worker.start().then(() => {
+// Just in development, we use MSW to mock API calls
+if (import.meta.env.DEV) {
+  import("./mocks/browser").then(({ worker }) => {
+    worker.start().then(() => {
+      renderApp();
+    });
+  });
+} else {
+  renderApp();
+}
+
+function renderApp() {
   ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
-});
+}
